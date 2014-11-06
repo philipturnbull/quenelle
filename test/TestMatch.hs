7c186a3bc97ee411abe1e700b4196e74e184ca81
Phil Turnbull <philip.turnbull@gmail.com>
2014-11-05 21:14:19 -0500

    Tidy up Match tests.

--- test/TestMatch.hs
+++ test/TestMatch.hs
@@ -4,7 +4,7 @@ module TestMatch (
 
 import Data.List
 import Language.Python.Common.AST
-import Language.Python.Common.Pretty
+import Language.Python.Common.Pretty (prettyText)
 import Language.Python.Version2.Parser
 import Test.HUnit.Base
 
@@ -28,33 +28,57 @@ testRule rulestr exprstr expected =
                         Left _ -> TestCase $ assertFailure $ "Failed to parse expr: " ++ exprstr
                         Right (expr, _) -> assertVars rulestr exprstr expected $ matchExprRule rule (normalizeExpr expr)
 
+t = testRule
+zero = Int 0 "0" ()
+one = Int 1 "1" ()
+add l r = BinaryOp (Plus ()) l r ()
+parens e = Paren e ()
+var s = Var (Ident s ()) ()
+
+testList name tests = TestLabel name $ TestList tests
 
 testMatchExprRule :: Test
-testMatchExprRule = TestLabel "matchExprRule" $ TestList [
+testMatchExprRule = testList "matchExprRule" [
+      testIntegers
+    , testParen
+    , testBinaryOp
+    , testUnaryOp
+    , testSubscript
+    , testCall
+    ]
+
+testIntegers = testList "Int/LongInt" [
       t "0" "0" [[]]
     , t "0" "1" []
     , t "0" "0x00000000" [[]]
     , t "8" "010" [[]]
     , t "0L" "0L" [[]]
-
     , t "E1" "0" [[("E1", zero)]]
-    , t "(E1)" "(0)" [[("E1", zero)]]
+    ]
+
+testParen = testList "Paren" [
+      t "(E1)" "(0)" [[("E1", zero)]]
     , t "(E1)" "0 + 1" []
     , t "E1" "((0))" [[("E1", parens $ parens zero)], [("E1", parens zero)], [("E1", zero)]]
-    , t "E1 + E2" "0 + 1" [[("E1", zero), ("E2", one)]]
-    , t "E1 + E1" "0 + 0" [[("E1", zero)]]
-    , t "-E1" "-1" [[("E1", one)]]
-    , t "-E1" "(-0) + (-1)" [[("E1", zero)], [("E1", one)]]
-    , t "E1()" "f()" [[("E1", var "f")]]
-    , t "E1()" "0()" [[("E1", zero)]]
+    ]
 
-    , t "E1[E1]" "0[0]" [[("E1", zero)]]
+testBinaryOp = testList "BinaryOp" [
+      t "E1 + E2" "0 + 1" [[("E1", zero), ("E2", one)]]
+    , t "E1 + E1" "0 + 0" [[("E1", zero)]]
+    ]
+
+testUnaryOp = testList "UnaryOp" [
+      t "-E1" "-1" [[("E1", one)]]
+    , t "-E1" "(-0) + (-1)" [[("E1", zero)], [("E1", one)]]
+    ]
+
+testSubscript = testList "Subscript" [
+      t "E1[E1]" "0[0]" [[("E1", zero)]]
     , t "E1[E2]" "0[1]" [[("E1", zero), ("E2", one)]]
     , t "E1[E1]" "0[1]" []
     ]
-    where t = testRule
-          zero = Int 0 "0" ()
-          one = Int 1 "1" ()
-          add l r = BinaryOp (Plus ()) l r ()
-          parens e = Paren e ()
-          var s = Var (Ident s ()) ()
+
+testCall = testList "Call" [
+      t "E1()" "f()" [[("E1", var "f")]]
+    , t "E1()" "0()" [[("E1", zero)]]
+    ]
